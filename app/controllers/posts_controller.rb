36b561affab1c52d20fe5f1ed4f6471f583b4af4
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-02-01 12:19:20 +0530

    Edit post functionality added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,12 +15,27 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post = Post.new(post_params)
 
     if @post.save
     redirect_to @post
     else
       render 'new'
-      end
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
